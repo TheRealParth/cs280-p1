0c22621dafca0c4b06ad19a9ff5c3f9efee5acf9
Parth Patel <psyrotix@gmail.com>
2017-02-14 23:24:15 -0500

    We have maps fellas

--- main.cpp
+++ main.cpp
@@ -10,16 +10,24 @@
 #include <string>
 #include <regex>
 #include <fstream>
+#include <map>
 
 using namespace std;
+
+map <string, map <string, int>> fileCountMap;
+map <char, map <string, map <string, int>>> charCountMap;
+map <string, map <string, map <string, int>>> wordCountMap;
+
+map <int, char> chars;
+map <int, string> words;
 
 string word;
 string findChar = "-findchar=";
 string findWord = "-findword=";
-int findCharMatchCount;
+
 int findCharCount = 0;
-int findWordMatchCount;
 int findWordCount = 0;
+int fileCount = 0;
 
 //----------------------------- ERROR HANDLERS -------------------------------
 int poorlyFormedError(string &arg){
@@ -37,6 +45,23 @@ int fileNotFoundError(string &filename){
 	return 0;
 }
 
+// ------------------------------ DOES EXIST FUNCTIONS ---------------------------------
+int doesExistChar(char &c){
+	for(int i = 0; i < findCharCount; i++){
+		if(chars[i] == c){
+			return 1;
+		}
+	}
+	return 0;
+}
+int doesExistWord(string &w){
+	for(int i = 0; i < findWordCount; i++){
+		if(!words[i].compare(w)){
+			return 1;
+		}
+	}
+	return 0;
+}
 // ------------------------------ GET WORDS ---------------------------------
 string *getWords(string &in){
     smatch sm;
@@ -63,8 +88,10 @@ string *getWords(string &in){
 
 // ----------------------------- FLAG HANDLER --------------------------------
 int flagHandler(string &flag){
+	int findCharMatchCount = 0;
+    int findWordMatchCount = 0;
 	string searchWord = "";
-	
+
 	for(int j = 0; j < flag.length(); j++){
 		if(flag[j] == findChar[j]){
 			findCharMatchCount++;
@@ -78,11 +105,13 @@ int flagHandler(string &flag){
 	    if(flag.length() != 11){
 	    	return poorlyFormedError(flag);
 	    } else {
-	    	findCharCount++;
-
-	    	cout << "Search letter: " << word[10] << endl;
-	    	// CHECK IF LETTER EXISTS 
-	    	// PUSH INTO LETTERS TO FIND.
+	    	int doesExist = doesExistChar(word[10]);
+	    	//add to chars map
+	    	if(!doesExist){
+	    		cout << "Search Char: " << word[10] << endl;
+	    		chars[findCharCount] = word[10];
+	    		findCharCount++;
+	    	}
 	    }
 	//If the flag is findword
 	} else if(findWordMatchCount == 10){
@@ -92,9 +121,14 @@ int flagHandler(string &flag){
 			for(int x = 10; x < word.length(); x++ ){
 				searchWord += word[x];
 			}
-			cout << "Search Word: " << searchWord << endl;
-			// CHECK IF WORD EXISTS 
-			//PUSH WORD INTO WORDS TO FIND.
+	    	int doesExist = doesExistWord(searchWord);
+	    	//add to chars map
+	    	if(!doesExist){
+	    		cout << "Search Word: " << searchWord << endl;
+	    		words[findWordCount] = searchWord;
+	    		findWordCount++;
+	    	}
+			
 		}
 	} else {
 		return notRecognizedError(flag);
@@ -102,25 +136,37 @@ int flagHandler(string &flag){
 	return 0;
 }
 // ------------------------------ FILE HANDLER ---------------------------------
-int fileHandler(string &filename){
-	return 0;
-}
+// int fileHandler(string &filename){
+// 	fileCount++;
+// 	return 0;
+// }
 
 int main(int argc, const char * argv[]) {
 
 
-    for(int i = 0; i < argc; i++){
-    	findCharMatchCount = 0;
-    	findWordMatchCount = 0;
+    for(int i = 1; i < argc; i++){
+
     	word = argv[i];
 
     	if(word[0] == '-'){
     		flagHandler(word);
 		} else {
-			fileHandler(word);
+			// fileHandler(word);
+			fileCount++;
 		}
 
     }
+
+    //FOR EVERY FILE
+    // READ FILE
+    // Search each line 
+    // Put lines into array
+    //
+		cout << "-----------------------------" << endl;
+		cout << "Word count: " << findWordCount << endl;
+		cout << "Letter count: " << findCharCount << endl;
+		cout << "File count: " << fileCount << endl;
+		cout << "-----------------------------" << endl;
 
 
     return 0;
