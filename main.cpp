4176d5d1b55edae0e1e6e25c05a48d69f5fb1420
Parth Patel <psyrotix@gmail.com>
2017-02-14 22:20:44 -0500

    Flag handling on point

--- main.cpp
+++ main.cpp
@@ -9,86 +9,115 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <fstream>
 
 using namespace std;
 
-// ----------------------------- PRINT DATA ---------------------------------
-// void *printData(int *lines, int *words, int *chars, string *fileName) {
-// 	cout << lines << " " << words << " " << fileName;
-// }
+string word;
+string findChar = "-findchar=";
+string findWord = "-findword=";
+int findCharMatchCount;
+int findCharCount = 0;
+int findWordMatchCount;
+int findWordCount = 0;
 
-// // ------------------------------ GET WORDS ---------------------------------
-// string *getWords(string in){
-//     smatch sm;
-//     regex wordReg ("\\s+"); //finds spaces, which we'll use to split up the words
-//     sregex_token_iterator reg_end;
+//----------------------------- ERROR HANDLERS -------------------------------
+int poorlyFormedError(string &arg){
+	cout << "Argument "  <<  arg << " is poorly formed" << endl;
+	return 0;
+}
 
-//     int i = 0;
-//     int tokenCount = 0;
-//     string tempy = "";
-//     for(sregex_token_iterator it(in.begin(), in.end(), wordReg, -1); it != reg_end; ++it) {
-//     	//check for matches here
+int notRecognizedError(string &arg){
+	cout << "Argument "  <<  arg << " is not recognized" << endl;
+	return 0;
+}
 
-//         i++;
-//     }
-//     string *newWords = new string[i];
-//     wordCount = i;
-    
-//     int j = 0;
-//     for(sregex_token_iterator it(in.begin(), in.end(), wordReg, -1); it != reg_end; ++it) {
-//         newWords[j] = it->str();
-//         j++;
-//     }
-//     return newWords;
-// }
+int fileNotFoundError(string &filename){
+	cout << "File " << filename << " is not found" << endl;
+	return 0;
+}
 
+// ------------------------------ GET WORDS ---------------------------------
+string *getWords(string &in){
+    smatch sm;
+    regex wordReg ("\\s+"); //finds spaces, which we'll use to split up the words
+    sregex_token_iterator reg_end;
+
+    int i = 0;
+    int tokenCount = 0;
+    string tempy = "";
+    for(sregex_token_iterator it(in.begin(), in.end(), wordReg, -1); it != reg_end; ++it) {
+        i++;
+    }
+    string *newWords = new string[i];
+    int wordCount = i;
+    int letterCount = 0;
+    int j = 0;
+    for(sregex_token_iterator it(in.begin(), in.end(), wordReg, -1); it != reg_end; ++it) {
+        newWords[j] = it->str();
+        letterCount += newWords[j].length();
+        j++;
+    }
+    return newWords;
+}
+
+// ----------------------------- FLAG HANDLER --------------------------------
+int flagHandler(string &flag){
+	string searchWord = "";
+	
+	for(int j = 0; j < flag.length(); j++){
+		if(flag[j] == findChar[j]){
+			findCharMatchCount++;
+		} 
+		if(word[j] == findWord[j]){
+			findWordMatchCount++;
+		}
+	}
+	//If the flag is findchar
+	if(findCharMatchCount == 10){
+	    if(flag.length() != 11){
+	    	return poorlyFormedError(flag);
+	    } else {
+	    	findCharCount++;
+
+	    	cout << "Search letter: " << word[10] << endl;
+	    	// CHECK IF LETTER EXISTS 
+	    	// PUSH INTO LETTERS TO FIND.
+	    }
+	//If the flag is findword
+	} else if(findWordMatchCount == 10){
+		if(flag.length() <= 10){
+			return poorlyFormedError(flag);
+		} else {
+			for(int x = 10; x < word.length(); x++ ){
+				searchWord += word[x];
+			}
+			cout << "Search Word: " << searchWord << endl;
+			// CHECK IF WORD EXISTS 
+			//PUSH WORD INTO WORDS TO FIND.
+		}
+	} else {
+		return notRecognizedError(flag);
+	}
+	return 0;
+}
+// ------------------------------ FILE HANDLER ---------------------------------
+int fileHandler(string &filename){
+	return 0;
+}
 
 int main(int argc, const char * argv[]) {
-	string word;
-	string searchWord;
-	string findChar = "-findchar=";
-	string findWord = "-findword=";
-	int findCharMatchCount;
-	int findCharCount = 0;
-	int findWordMatchCount;
-	int findWordCount = 0;
+
 
     for(int i = 0; i < argc; i++){
     	findCharMatchCount = 0;
+    	findWordMatchCount = 0;
     	word = argv[i];
 
     	if(word[0] == '-'){
-	        for(int j = 0; j < word.length(); j++){
-	        	if(word[j] == findChar[j]){
-	        		findCharMatchCount++;
-	        	} 
-	        	if(word[j] == findWord[j]){
-	        		findWordMatchCount++;
-	        	}
-	        }
-	        if(findCharMatchCount == 10){
-		        if(word.length() != 11){
-		        	cout << "Invalid entry for findchar. Expected char.";
-		        	return 0;
-		        } else {
-		        	findCharCount++;
-		        	cout<< word[10] << endl;
-		        	// PUSH INTO LETTERS TO FIND.
-		        }
-		    } else if(findWordMatchCount == 10){
-		    	if(word.length() <= 10){
-		    		cout << "Invalid entry for findword. Expected word";
-		    		return 0;
-		    	} else {
-		    		for(int x = 10; x < word.length(); x++ ){
-		    			searchWord += word[x];
-		    		}
-		    		cout << searchWord;
-		    	}
-		    } else {
-		    	cout << "Invalid flag";
-		    	return 0;
-		    }
+    		flagHandler(word);
+		} else {
+			fileHandler(word);
 		}
 
     }
